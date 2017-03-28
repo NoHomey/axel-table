ec33400d34a49ac9d6ffe6e6e9da634a2f93e906
NoHomey <ivo.stratev.tues@gmail.com>
2017-03-28 18:13:55 +0300

    passign all tests

--- src/StringObject/StringObject.cc
+++ src/StringObject/StringObject.cc
@@ -36,9 +36,23 @@ char StringObject::operator[](const size_t index) const noexcept {
 }
 
 bool StringObject::operator==(const StringObject& other) const noexcept {
-    return false;
+    if(string == nullptr) {
+        return (other.string == nullptr) || (other.string[0] == '\0');
+    }
+    if(other.string == nullptr) {
+        return (string == nullptr) || (string[0] == '\0');
+    }
+    size_t index = 0;
+    while(true) {
+        if(string[index] != other.string[index]) {
+            return false;
+        } else if(string[index] == '\0') {
+            return true;
+        }
+        ++index;
+    }
 }
 
 bool StringObject::operator!=(const StringObject& other) const noexcept {
-    return false;
+    return !(*this == other);
 }

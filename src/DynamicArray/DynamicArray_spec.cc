778bbabe61b240e6f554328650bc3272b0274506
NoHomey <ivo.stratev.tues@gmail.com>
2017-06-10 14:42:05 +0300

    implementing size constructor to DynamicArray

--- src/DynamicArray/DynamicArray_spec.cc
+++ src/DynamicArray/DynamicArray_spec.cc
@@ -6,9 +6,17 @@ template class DynamicArray<int>;
 using Array = DynamicArray<int>;
 
 TEST(DynamicArray, DefaultConstructor) {
-    IT("should create empty array");
+    IT("should create empty array with no capacity");
     Array array;
     EXPECT_TRUE(array.isEmpty());
     EXPECT_EQ(array.size(), 0);
     EXPECT_EQ(array.capacity(), 0);
+}
+
+TEST(DynamicArray, SizeConstructor) {
+    IT("should create empty array with capacity equal to provided size");
+    Array array{9};
+    EXPECT_TRUE(array.isEmpty());
+    EXPECT_EQ(array.size(), 0);
+    EXPECT_EQ(array.capacity(), 9);
 }

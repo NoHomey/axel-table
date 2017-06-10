61c9f009980914c42381a5825e9990fd983533a1
NoHomey <ivo.stratev.tues@gmail.com>
2017-06-10 19:08:28 +0300

    spliting old tests

--- src/DynamicArray/DynamicArray_spec.cc
+++ src/DynamicArray/DynamicArray_spec.cc
@@ -22,10 +22,74 @@ TEST(DynamicArray, SizeConstructor) {
     EXPECT_EQ(array.capacity(), 9);
     EXPECT_FALSE(array.isFull());
     EXPECT_THROW(array.pop(), ::EmptyDynamicArray);
+}
+
+TEST(DynamicArray, isEmpty) {
+    IT("returns true if array's size is equal to 0");
+    TestArray array;
+    EXPECT_TRUE(array.isEmpty());
+    array.push(9);
+    EXPECT_FALSE(array.isEmpty());
+}
+
+TEST(DynamicArray, isFull) {
+    IT("returns true if array's size is equal to it's capacity");
+    TestArray array;
+    EXPECT_TRUE(array.isFull());
+    array.push(9);
+    EXPECT_FALSE(array.isFull());
+    EXPECT_EQ(array.size(), 1);
+    EXPECT_EQ(array.capacity(), 2);
+}
+
+TEST(DynamicArray, size) {
+    IT("returns the count of elements in the array");
+    TestArray array;
+    EXPECT_EQ(array.size(), 0);
+    array.push(9);
+    EXPECT_EQ(array.size(), 1);
+    array.pop();
+    EXPECT_EQ(array.size(), 0);
+    array.push(4);
+    array.push(2);
+    EXPECT_EQ(array.size(), 2);
+    array.pop();
+    for(size_t i = 0; i < 9; ++i) {
+        array.push(i);
+    }
+    EXPECT_EQ(array.size(), 10);
+    array.pop();
+    EXPECT_EQ(array.size(), 9);
+}
+
+TEST(DynamicArray, capacity) {
+    IT("returns the size of dynamically allocated memory for the array's elements");
+    TestArray array;
+    EXPECT_EQ(array.capacity(), 0);
+    array.push(9);
+    EXPECT_EQ(array.capacity(), 2);
+    array.pop();
+    EXPECT_EQ(array.capacity(), 2);
+    array.push(4);
+    array.push(2);
+    array.push(1);
+    EXPECT_EQ(array.capacity(), 6);
+    array.pop();
+    for(size_t i = 0; i < 9; ++i) {
+        array.push(i);
+    }
+    EXPECT_EQ(array.capacity(), 14);
+    array.pop();
+    EXPECT_EQ(array.capacity(), 14);
+}
+
+TEST(DynamicArray, push) {
+    IT("adds new element to the array if it is full it will also resize it to capacity equal to [2 x size]");
+    TestArray array;
     array.push(9);
     EXPECT_FALSE(array.isEmpty());
     EXPECT_EQ(array.size(), 1);
-    EXPECT_EQ(array.capacity(), 9);
+    EXPECT_EQ(array.capacity(), 2);
     EXPECT_FALSE(array.isFull());
     EXPECT_NO_THROW(array.pop());
     for(size_t i = 0; i < 9; ++i) {
@@ -33,27 +97,91 @@ TEST(DynamicArray, SizeConstructor) {
     }
     EXPECT_FALSE(array.isEmpty());
     EXPECT_EQ(array.size(), 9);
-    EXPECT_EQ(array.capacity(), 9);
-    EXPECT_TRUE(array.isFull());
+    EXPECT_EQ(array.capacity(), 14);
+    EXPECT_FALSE(array.isFull());
     EXPECT_NO_THROW(array.pop());
     for(size_t i = 42; i < 47; ++i) {
         array.push(i);
     }
     EXPECT_FALSE(array.isEmpty());
     EXPECT_EQ(array.size(), 13);
-    EXPECT_EQ(array.capacity(), 18);
+    EXPECT_EQ(array.capacity(), 14);
+    EXPECT_FALSE(array.isFull());
+    array.push(22);
+    EXPECT_EQ(array.size(), 14);
+    EXPECT_EQ(array.capacity(), 14);
+    EXPECT_TRUE(array.isFull());
+    array.push(23);
+    EXPECT_EQ(array.size(), 15);
+    EXPECT_EQ(array.capacity(), 30);
     EXPECT_FALSE(array.isFull());
     EXPECT_NO_THROW(array.pop());
+}
+
+TEST(DynamicArray, shrinkToFit) {
+    IT("if not full allocates new array with capacity equal to size the copy all elements, dealocates old memory and moves assigns it");
+    TestArray array;
+    EXPECT_EQ(array.capacity(), 0);
+    array.push(9);
+    EXPECT_EQ(array.capacity(), 2);
+    EXPECT_EQ(array.size(), 1);
     array.shrinkToFit();
+    EXPECT_EQ(array.size(), 1);
+    EXPECT_EQ(array.capacity(), 1);
+    array.push(4);
+    array.push(2);
+    array.push(1);
+    EXPECT_EQ(array.capacity(), 4);
+    EXPECT_EQ(array.size(), 4);
+    array.shrinkToFit();
+    EXPECT_EQ(array.size(), 4);
+    EXPECT_EQ(array.capacity(), 4);
+    for(size_t i = 0; i < 9; ++i) {
+        array.push(i);
+    }
+     EXPECT_EQ(array.capacity(), 22);
+    EXPECT_EQ(array.size(), 13);
+    array.shrinkToFit();
+    EXPECT_EQ(array.size(), 13);
+    EXPECT_EQ(array.capacity(), 13);
+}
+
+TEST(DynamicArray, clear) {
+    IT("clears array content and resets size and capacity");
+    TestArray array;
+    EXPECT_TRUE(array.isEmpty());
+    array.push(9);
+    EXPECT_EQ(array.capacity(), 2);
+    EXPECT_EQ(array.size(), 1);
     EXPECT_FALSE(array.isEmpty());
-    EXPECT_EQ(array.size(), 12);
-    EXPECT_EQ(array.capacity(), 12);
-    EXPECT_TRUE(array.isFull());
-    EXPECT_NO_THROW(array.pop());
+    EXPECT_FALSE(array.isFull());
     array.clear();
     EXPECT_TRUE(array.isEmpty());
     EXPECT_EQ(array.size(), 0);
     EXPECT_EQ(array.capacity(), 0);
     EXPECT_TRUE(array.isFull());
-    EXPECT_THROW(array.pop(), ::EmptyDynamicArray);
+    array.push(4);
+    array.push(2);
+    array.push(1);
+    EXPECT_EQ(array.capacity(), 6);
+    EXPECT_EQ(array.size(), 3);
+    EXPECT_FALSE(array.isEmpty());
+    EXPECT_FALSE(array.isFull());
+    array.clear();
+    EXPECT_TRUE(array.isEmpty());
+    EXPECT_EQ(array.size(), 0);
+    EXPECT_EQ(array.capacity(), 0);
+    EXPECT_TRUE(array.isFull());
+    for(size_t i = 0; i < 9; ++i) {
+        array.push(i);
+    }
+    EXPECT_EQ(array.capacity(), 14);
+    EXPECT_EQ(array.size(), 9);
+    EXPECT_FALSE(array.isEmpty());
+    EXPECT_FALSE(array.isFull());
+    array.clear();
+    EXPECT_TRUE(array.isEmpty());
+    EXPECT_EQ(array.size(), 0);
+    EXPECT_EQ(array.capacity(), 0);
+    EXPECT_TRUE(array.isFull());;
 }

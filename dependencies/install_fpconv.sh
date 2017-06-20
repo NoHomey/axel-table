2b4462f0cf6a95c6ab77ed4102de5464066f110d
NoHomey <ivo.stratev.tues@gmail.com>
2017-06-20 10:31:24 +0300

    adding depencies setup scripts and copyright notice for external dependencies

---
+++ dependencies/install_fpconv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for f in ./fpconv/src/*
+do
+    cp ./fpconv/license copyright
+    sed -i '1s/^/\/*\n\n/' copyright
+    echo $'\n*/\n' >> copyright
+    cat $f >> copyright
+    rm $f
+    mv copyright $f
+done

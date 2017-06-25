5a217f10b7d6909b4af29938daacec89ca20b8ff
NoHomey <ivo.stratev.tues@gmail.com>
2017-06-25 19:55:11 +0300

    deleting ctors of ConstString

--- src/String/ConstString/ConstString.h
+++ src/String/ConstString/ConstString.h
@@ -7,6 +7,18 @@ class ConstCharString: public BasicString<const char*> {
 public:
     class BadStringOffset: public Exception { };
 
+    ConstCharString() noexcept = delete;
+
+    ~ConstCharString() noexcept = default;
+
+    ConstCharString(const ConstCharString&) noexcept = delete;
+
+    ConstCharString(ConstCharString&&) = delete;
+
+    ConstCharString& operator=(const ConstCharString&) = delete;
+
+    ConstCharString& operator=(ConstCharString&&) = delete;
+
     ConstCharString(const char* cstring, const size_t cstrLength) noexcept;
 
     ConstCharString(const ConstCharString& other, const size_t offsetFromBegging);
